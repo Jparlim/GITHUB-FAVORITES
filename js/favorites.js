ea3885207b404caeda192f691ad4362f501e0a22
Jparlim <Gusjao0500@gmail.com>
2024-07-02 12:36:08 -0300

    upgrad functions

--- js/favorites.js
+++ js/favorites.js
@@ -1,7 +1,69 @@
+export class githubusers {
+    static search(username) {
+        const link = `https://api.github.com/users/${username}`
+        return fetch(link)
+        .then(data => data.json())
+        .then(({login, name, public_repos, followers}) => ({
+            login,
+            name,
+            public_repos,
+            followers,
+        }))
+    }
+}
+
 class favorites{
     constructor(root) {
         this.root = document.querySelector(root)
         this.load()
+        this.onadd()
+    }
+
+    save() {
+        localStorage.setItem('@github-favorite:', JSON.stringify(this.users))
+    }
+
+    async add(username) {
+        //try = tente fazer esta função, se algo der errado vai no catch
+        try {
+            const user = await githubusers.search(username)
+            if(user.login === undefined) {
+                //o new Error vai sempre procurar o catch
+                throw new Error('encontrei um erro!!')
+            }
+
+            this.users = [user, ...this.users]
+            this.update()
+            this.save()
+        } catch(ERROR) {
+            alert(ERROR.message)
+        }
+    }
+
+    onadd() {
+        const addbutton = this.root.querySelector('.search button')
+        addbutton.onclick = () => {
+            const { value } = this.root.querySelector('.search input')
+            this.add(value)
+        }
+    }
+
+    load() {
+        
+        /*json = quando colocado no localstorage, tudo vira string
+        o json é usado para tirar tirar do modo string.
+        localstorage = é uma pequena memoria da web, onde armazena os dados colocados por mim
+        getitem = puxa um dado que foi armazenado no localstorage.*/
+
+        const users = JSON.parse(localStorage.getItem('@github-favorite:')) || []
+        this.users = []
+    }
+
+    delete(user) {
+        const entrysfilter = this.users.filter(entry => entry.login !== user.login)
+        this.users = entrysfilter
+        this.update()
+        this.save()
     }
 }
 
@@ -12,25 +74,6 @@ export class favoritesview extends favorites {
         this.update()
     }
 
-
-    load() {
-        this.users = 
-        [
-            {
-                login: 'maykbrito',
-                name: 'mayk brito',
-                public_repost: '76',
-                followers: '120000',
-            },
-            {
-                login: 'diego3g',
-                name: 'diego fernandes',
-                public_repost: '48',
-                followers: '22503',
-            }
-        ]
-    }
-
     update() {
         this.removeAlltr()
 
@@ -39,9 +82,16 @@ export class favoritesview extends favorites {
             row.querySelector('.user img').src = `https://github.com/${user.login}.png`
             row.querySelector('.user p').textContent = user.name
             row.querySelector('.user span').textContent = user.login
-            row.querySelector('.repositories').textContent = user.public_repost
+            row.querySelector('.repositories').textContent = user.public_repos
             row.querySelector('.followers').textContent = user.followers
-            
+        
+            row.querySelector('.buttonX').onclick = () => {
+                const isOK = confirm('deseja excluir este perfil da sua lista de favoritos?')
+                if(isOK) {
+                    this.delete(user)
+                }
+            }
+
             this.tbody.append(row)
         })
     }
